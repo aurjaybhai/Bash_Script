925f6b248480d51ebbd62a367a0e7e192d5b635f
aurjaybhai <jaypardule10@gmail.com>
2025-08-18 00:54:28 +0530

    just modified the comments

--- newconcepts/menu.sh
+++ newconcepts/menu.sh
@@ -4,7 +4,9 @@ PS3="Select an Option : " #ps3 is special variable in bash scripting
 
 options=("Ubuntu" "Arch" "Linux Mint" "Fedora" "Quit")
 
-select os in "${options[@]}"; do
+select os in "${options[@]}"; do #Output: Linux Mint Red Hat Ubuntu (3 separate arguments) don't get confuse see the below line
+    #echo "${options[*]}"  ======>  #### Both @ and * mean "all elements in the array" but they behave differently when quoted:#### <=======
+    # Output: Linux Mint Red Hat Ubuntu (1 single argument)
     case $os in "Ubuntu")
         echo "You choosen $os"
         break
