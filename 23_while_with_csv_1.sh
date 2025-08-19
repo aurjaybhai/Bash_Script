50a4a9bd16a6675241a7a17cf6991bbc6167bd9c
aurjaybhai <jaypardule10@gmail.com>
2025-08-19 15:41:00 +0000

    clarity more about Input Operators in the file 23_while_with_csv

--- 23_while_with_csv_1.sh
+++ 23_while_with_csv_1.sh
@@ -5,3 +5,10 @@ while IFS="," read id name age; do # IFS= Internal Field Seperator
     echo "name is $name"
     echo "age is $age"
 done <test.csv
+#######################################
+
+# The three Different Input Operators
+
+# (1)  < - Input Redirection(from file)
+# (2) << -multiple lines of input directly in the script
+# (3) <<< - content of a variable or string and feeds it as input
